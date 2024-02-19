1d1155b7c4a7fbba16ac83e06f16512255e9e256
kintal2026 <38499600+DatBlueDev@users.noreply.github.com>
2024-02-19 09:56:07 +0800

    splitting main class into two MDAA and CheckWin

--- MDAA.java
+++ MDAA.java
@@ -4,105 +4,79 @@
 
 import java.util.*;
 
-public class MDAA{
+public class MDAA {
     static final int boardLength = 7;
-    static final int boardHeight = 6; 
+    static final int boardHeight = 6;
     static int insertTokenIndex;
     static int turnNumber = 1;
     static int currentPlayer = 1;
+    static int[] savePos = new int[2];
     static boolean running = true;
-    static int boardArray[][]
-     = {{0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0}};
-    static int [] savePos = new int[2];
-    static int [][] scanPos = new int[8][2];
+    static int boardArray[][] = { { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 } };
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         int n;
-        while (running){
-            currentPlayer = turnNumber % 2 == 1? 1:2;
+        while (running) {
+            currentPlayer = turnNumber % 2 == 1 ? 1 : 2;
             System.out.println("It is player " + currentPlayer + "'s turn");
             displayBoard();
             n = reader.nextInt();
             dropTokens(n);
             System.out.println("");
-            checkWinCondition();
-            turnNumber+=1;
+            running = checkWinCondition.checkWin() == 0 ? true : false;
+            if (!running) {
+                System.out.println("PLayer " + checkWinCondition.winPlayer + " won!");
+            }
+            turnNumber += 1;
         }
 
     }
 
-    public static void resetBoard(){
-        boardArray[0] = new int[] {0, 0, 0, 0, 0, 0, 0};
-        boardArray[1] = new int[] {0, 0, 0, 0, 0, 0, 0};
-        boardArray[2] = new int[] {0, 0, 0, 0, 0, 0, 0};
-        boardArray[3] = new int[] {0, 0, 0, 0, 0, 0, 0};
-        boardArray[4] = new int[] {0, 0, 0, 0, 0, 0, 0};
-        boardArray[5] = new int[] {0, 0, 0, 0, 0, 0, 0};
+    public static void resetBoard() {
+        boardArray[0] = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+        boardArray[1] = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+        boardArray[2] = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+        boardArray[3] = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+        boardArray[4] = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+        boardArray[5] = new int[] { 0, 0, 0, 0, 0, 0, 0 };
 
     }
 
-    public static void displayBoard(){
-        for (int i = 0; i<boardHeight; i++){
-            for (int j = 0; j<boardLength; j++){
+    public static void displayBoard() {
+        for (int i = 0; i < boardHeight; i++) {
+            for (int j = 0; j < boardLength; j++) {
                 System.out.print(boardArray[i][j]);
             }
             System.out.println("");
         }
     }
 
-    public static void dropTokens(int columnIndex){
+    public static void dropTokens(int columnIndex) {
         boolean allowToPlace = true;
-        for (int i = 0; i < boardHeight; i++){
-            if ((boardArray[i][columnIndex] != 0) && i > 0){
-                insertTokenIndex = i-1;
-                
+        for (int i = 0; i < boardHeight; i++) {
+            if ((boardArray[i][columnIndex] != 0) && i > 0) {
+                insertTokenIndex = i - 1;
+
                 break;
-            }
-            else if ((boardArray[i][columnIndex] != 0) &&i == 0) {
+            } else if ((boardArray[i][columnIndex] != 0) && i == 0) {
                 System.out.println("Column Full");
                 allowToPlace = false;
                 break;
-            }
-            else{
-                insertTokenIndex=boardHeight-1;
+            } else {
+                insertTokenIndex = boardHeight - 1;
             }
         }
-        if (allowToPlace){
+        if (allowToPlace) {
             boardArray[insertTokenIndex][columnIndex] = currentPlayer;
             savePos[0] = insertTokenIndex;
             savePos[1] = columnIndex;
         }
 
-
-    } 
-
-    public static int checkWinCondition(){
-        scanPos[0][0] = savePos[0] - 3 < 0? 0:savePos[0] - 3;//first point scan vertical
-        scanPos[0][1] = savePos[1];
-        scanPos[1][0] = savePos[0] + 3 > 5? 5:savePos[0] + 3;//second point scan vertical
-        scanPos[1][1] = savePos[1];
-        scanPos[2][0] = savePos[0];//first point scan horizontal
-        scanPos[2][1] = savePos[1] - 3 < 0? 0:savePos[1] - 3;
-        scanPos[3][0] = savePos[0];//second point scan horizontal
-        scanPos[3][1] = savePos[1] + 3 > 6? 6:savePos[1] + 3;
-        scanPos[4][0] = savePos[0] - 3 < 0? 0:savePos[0] - 3;//first point scan diagonal negative slope
-        scanPos[4][1] = savePos[1] - 3 < 0? 0:savePos[1] - 3;
-        scanPos[5][0] = savePos[0] + 3 > 5? 5:savePos[0] + 3;//second point scan diagonal negative slope
-        scanPos[5][1] = savePos[1] + 3 > 6? 6:savePos[1] + 3;
-        scanPos[6][0] = savePos[0] + 3 > 5? 5:savePos[0] + 3;//first point scan diagonal positive slope
-        scanPos[6][1] = savePos[1] - 3 < 0? 0:savePos[1] - 3;
-        scanPos[7][0] = savePos[0] - 3 < 0? 0:savePos[0] - 3;//second point scan diagonal positive slope
-        scanPos[7][1] = savePos[1] + 3 > 6? 6:savePos[1] + 3;
-        for(int i = 0; i < 8; i++){
-            System.out.println(scanPos[i][0] + ", " + scanPos[i][1]);
-        }
-        return 0;
     }
 }
-    
---
+++ checkWinCondition.java
@@ -0,0 +1,117 @@
+public class checkWinCondition {
+    static int[][] scanPos = new int[8][2];
+    static int scanCounterV = 0;
+    static int scanCounterH = 0;
+    static int scanCounterND = 0;
+    static int scanCounterPD = 0;
+    static int winPlayer = 0;
+
+    public static int checkWin() {
+        scanPosGenerator();
+
+        // scan coordinates
+        for (int i = 0; i < 8; i++) {
+            System.out.println(scanPos[i][0] + ", " + scanPos[i][1]);
+        }
+
+        winConditionScan();
+
+        if (winPlayer != 0) {
+            return winPlayer;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void scanPosGenerator() {
+        scanPos[0][0] = MDAA.savePos[0];// first point scan vertical
+        scanPos[0][1] = MDAA.savePos[1];
+        scanPos[1][0] = MDAA.savePos[0] + 3 > 5 ? 5 : MDAA.savePos[0] + 3;// second point scan vertical
+        scanPos[1][1] = MDAA.savePos[1];
+        scanPos[2][0] = MDAA.savePos[0];// first point scan horizontal
+        scanPos[2][1] = MDAA.savePos[1] - 3 < 0 ? 0 : MDAA.savePos[1] - 3;
+        scanPos[3][0] = MDAA.savePos[0];// second point scan horizontal
+        scanPos[3][1] = MDAA.savePos[1] + 3 > 6 ? 6 : MDAA.savePos[1] + 3;
+        scanPos[4][0] = MDAA.savePos[0] - 3 < 0 ? 0 : MDAA.savePos[0] - 3;// first point scan diagonal negative slope
+        scanPos[4][1] = MDAA.savePos[1] - 3 < 0 ? 0 : MDAA.savePos[1] - 3;
+        if ((MDAA.savePos[0] - scanPos[4][0]) > (MDAA.savePos[1] - scanPos[4][1])) {
+            scanPos[4][0] = scanPos[4][0] + ((MDAA.savePos[0] - scanPos[4][0]) - (MDAA.savePos[1] - scanPos[4][1]));
+        } else if ((MDAA.savePos[0] - scanPos[4][0]) < (MDAA.savePos[1] - scanPos[4][1])) {
+            scanPos[4][1] = scanPos[4][1] + ((MDAA.savePos[1] - scanPos[4][1]) - (MDAA.savePos[0] - scanPos[4][0]));
+        }
+        scanPos[5][0] = MDAA.savePos[0] + 3 > 5 ? 5 : MDAA.savePos[0] + 3;// second point scan diagonal negative slope
+        scanPos[5][1] = MDAA.savePos[1] + 3 > 6 ? 6 : MDAA.savePos[1] + 3;
+        if ((scanPos[5][0] - MDAA.savePos[0]) > (scanPos[5][1] - MDAA.savePos[1])) {
+            scanPos[5][0] = scanPos[5][0] - ((scanPos[5][0] - MDAA.savePos[0]) - (scanPos[5][1] - MDAA.savePos[1]));
+        } else if ((scanPos[5][0] - MDAA.savePos[0]) < (scanPos[5][1] - MDAA.savePos[1])) {
+            scanPos[5][1] = scanPos[5][1] - ((scanPos[5][1] - MDAA.savePos[1]) - (scanPos[5][0] - MDAA.savePos[0]));
+        }
+        scanPos[6][0] = MDAA.savePos[0] + 3 > 5 ? 5 : MDAA.savePos[0] + 3;// first point scan diagonal positive slope
+        scanPos[6][1] = MDAA.savePos[1] - 3 < 0 ? 0 : MDAA.savePos[1] - 3;
+        if ((scanPos[6][0] - MDAA.savePos[0]) > (MDAA.savePos[1] - scanPos[6][1])) {
+            scanPos[6][0] = scanPos[6][0] - ((scanPos[6][0] - MDAA.savePos[0]) - (MDAA.savePos[1] - scanPos[6][1]));
+        } else if ((scanPos[6][0] - MDAA.savePos[0]) < (MDAA.savePos[1] - scanPos[6][1])) {
+            scanPos[6][1] = scanPos[6][1] + ((MDAA.savePos[1] - scanPos[6][1]) - (scanPos[6][0] - MDAA.savePos[0]));
+        }
+        scanPos[7][0] = MDAA.savePos[0] - 3 < 0 ? 0 : MDAA.savePos[0] - 3;// second point scan diagonal positive slope
+        scanPos[7][1] = MDAA.savePos[1] + 3 > 6 ? 6 : MDAA.savePos[1] + 3;
+        if ((MDAA.savePos[0] - scanPos[7][0]) > (scanPos[7][1] - MDAA.savePos[1])) {
+            scanPos[7][0] = scanPos[7][0] + (MDAA.savePos[0] - scanPos[7][0]) - (scanPos[7][1] - MDAA.savePos[1]);
+        } else if ((MDAA.savePos[0] - scanPos[7][0]) < (scanPos[7][1] - MDAA.savePos[1])) {
+            scanPos[7][1] = scanPos[7][1]
+                    - ((scanPos[7][1] - MDAA.savePos[1]) - (MDAA.savePos[0] - scanPos[7][0]));
+        }
+    }
+
+    public static void winConditionScan() {
+        // vertical line scan
+        if (!((scanPos[0][0]) > 2)) {
+            for (int i = scanPos[0][0]; i <= scanPos[1][0]; i++) {
+                scanCounterV = MDAA.boardArray[i][scanPos[0][1]] == MDAA.currentPlayer ? scanCounterV + 1 : 0;
+
+                if (scanCounterV >= 4) {
+                    winPlayer = MDAA.currentPlayer;
+                    break;
+                }
+            }
+        }
+
+        // horizontal line scan
+        for (int i = scanPos[2][1]; i <= scanPos[3][1]; i++) {
+            scanCounterH = MDAA.boardArray[scanPos[2][0]][i] == MDAA.currentPlayer ? scanCounterH + 1 : 0;
+
+            if (scanCounterH >= 4) {
+                winPlayer = MDAA.currentPlayer;
+                break;
+            }
+        }
+
+        // negative slope diagonal line scan
+        if (scanPos[5][0] - scanPos[4][0] > 2) {
+            for (int i = scanPos[4][0]; i <= scanPos[5][0]; i++) {
+                scanCounterND = MDAA.boardArray[i][scanPos[4][1] + (i - scanPos[4][0])] == MDAA.currentPlayer
+                        ? scanCounterND + 1
+                        : 0;
+                if (scanCounterND >= 4) {
+                    winPlayer = MDAA.currentPlayer;
+                    break;
+                }
+            }
+        }
+
+        // positive slope diagonal line scan
+        if (scanPos[6][0] - scanPos[7][0] > 2) {
+            for (int i = scanPos[6][0]; i >= scanPos[7][0]; i--) {
+                scanCounterPD = MDAA.boardArray[i][scanPos[6][1] + (scanPos[6][0] - i)] == MDAA.currentPlayer
+                        ? scanCounterPD + 1
+                        : 0;
+                System.out.println(scanCounterPD);
+                if (scanCounterPD >= 4) {
+                    winPlayer = MDAA.currentPlayer;
+                    break;
+                }
+            }
+        }
+    }
+
+}
